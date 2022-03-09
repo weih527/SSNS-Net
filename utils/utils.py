1879251a499bcf459f2e809de1753354c5bf8206
Wei Huang <44992496+weih527@users.noreply.github.com>
2022-03-09 21:45:53 +0800

    Add files via upload

---
+++ utils/utils.py
@@ -0,0 +1,57 @@
+import torch
+import random
+import numpy as np
+import subprocess
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    # torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+def execute(cmd):
+    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
+
+def compute_num_single(size, stride):
+    # 计算大概需要滑窗的个数
+    num_window = size // stride
+    # 判断是否能整除，如果不可以就+1
+    # 即滑窗个数加1
+    if size % stride != 0:
+        num_window += 1
+    # 计算padding的大小
+    padding_2times = num_window * stride - size + (stride * 2)
+    # 如果padding的大小不能被2整除
+    # 就继续增加滑窗的个数，已使得padding_2times能被2整除
+    while padding_2times % 2 != 0:
+        num_window += 1
+        padding_2times = num_window * stride - size + (stride * 2)
+    # 除以2是为了对称padding
+    padding = padding_2times // 2
+    # 加1是为了补上最后一个完整的滑窗
+    num_window += 1
+    return num_window, padding
+
+def compute_num(raw_shape, stride):
+    size_z = raw_shape[0]
+    size_xy = raw_shape[1]
+    stride_z = stride[0]
+    stride_xy = stride[1]
+    num_z, padding_z = compute_num_single(size_z, stride_z)
+    num_xy, padding_xy = compute_num_single(size_xy, stride_xy)
+    return [num_z, num_xy, num_xy], [padding_z, padding_xy, padding_xy]
+
+if __name__ == "__main__":
+    raw = [500, 4096, 4096]
+    stride = [18, 128, 128]
+    num, padding = compute_num(raw, stride)
+    print(num)
+    print(padding)
